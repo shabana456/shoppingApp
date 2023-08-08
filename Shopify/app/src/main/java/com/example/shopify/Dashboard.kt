d042c02c94a72462e5b501997d65f41f1299dc6f
shabana456 <Rozeline@786>
2023-08-09 01:43:30 +0530

    code changes fragment

--- Shopify/app/src/main/java/com/example/shopify/Dashboard.kt
+++ Shopify/app/src/main/java/com/example/shopify/Dashboard.kt
@@ -4,21 +4,55 @@ package com.example.shopify
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 
-class Dashboard : AppCompatActivity() {
+
+class Dashboard : AppCompatActivity(){
+
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        val btnDisplay = findViewById<Button>(R.id.btnPayment)
-        btnDisplay.setOnClickListener(){
-            val intent = Intent(this, Payment::class.java)
-            startActivity(intent)
+        val btFragmentOne = findViewById<Button>(R.id.frament1btn)
+        val btFragmentTwo = findViewById<Button>(R.id.frament2btn)
+        val btFragmentThree = findViewById<Button>(R.id.frament3btn)
+        //btnfrag1Display.setOnClickListener(this)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, KFragment.newInstance("", ""))
+            commit()
+            addToBackStack(null)
+        }
+        btFragmentOne.setOnClickListener{
+            supportFragmentManager.beginTransaction().
+            apply {
+                replace(R.id.frameLayout,MFragment.newInstance("",""))
+                commit()
+                addToBackStack(null)
+            }
+        }
+
+        btFragmentTwo.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayout,WFragment.newInstance("",""))
+                commit()
+                addToBackStack(null)
+            }
+        }
+        btFragmentThree.setOnClickListener{
+            supportFragmentManager.beginTransaction().
+            apply {
+                replace(R.id.frameLayout,KFragment.newInstance("",""))
+                commit()
+                addToBackStack(null)
+            }
         }
     }
+
+
 }
